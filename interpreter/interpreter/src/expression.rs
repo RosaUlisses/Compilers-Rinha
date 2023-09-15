0ff9bab4aa10f7e6e63d91bcf6ad66b2646b71bf
RosaUlisses <ulissao@proton.me>
2023-09-15 10:35:23 -0300

    feat: implement the expression enum

---
+++ interpreter/interpreter/src/expression.rs
@@ -0,0 +1,30 @@
+enum BinaryOperator {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+    Remainder,
+    Equal,
+    NotEqual,
+    LessThan,
+    GreaterThanEqual,
+    LessThanEqual,
+    And,
+    Or,
+}
+
+enum LiteralValue {
+   Int(i32), 
+   Str(String),
+   Boolean(bool), 
+}
+
+enum Expression {
+    If {condition: Box<Expression>, then_branch: Box<Expression>, else_branch: Box<Expression>},
+    FunctionDeclaration {parameters: Vec<String>, body: Box<Expression>},
+    Call {calee: Box<Expression>, arguments: Vec<Box<Expression>>},
+    VarDeclaration {name: String, value: Box<Expression>, next: Box<Expression>},
+    Binary {left: Box<Expression>, operator: BinaryOperator, right: Box<Expression>},
+    Tuple {first: Box<Expression>, second: Box<Expression>},
+    Literal {value: LiteralValue}
+}
